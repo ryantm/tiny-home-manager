275f33b22884292f69c0380265383ff78a0509f3
Ryan Mulligan <ryan@ryantm.com>
2020-07-11 07:48:48 -0700

    tiny-home-manager
    use one file to manage home

--- shell.nix
+++ shell.nix
@@ -1,23 +1,40 @@
 let
 
-  sources = import ./nix/sources.nix;
+  nixpkgs-src = builtins.fetchTarball { url = "https://github.com/nixos/nixpkgs/archive/master.tar.gz"; };
+  pkgs = import nixpkgs-src {};
 
-  nixpkgs = sources."nixpkgs-unstable";
+  home-manager-src = builtins.fetchTarball { url = "https://github.com/rycee/home-manager/archive/master.tar.gz"; };
+  home-manager = (import home-manager-src {inherit pkgs;}).home-manager;
 
-  pkgs = import nixpkgs {};
+  home.nix = ''
+    { pkgs, ... }:
+    {
+      home.username = "$USER";
+      home.homeDirectory = "$HOME";
+      home.stateVersion = "20.09";
+
+      programs.bash = {
+        enable = true;
+      };
+
+      home.packages = [
+        pkgs.cowsay
+      ];
+    }
+  '';
 
 in pkgs.mkShell rec {
 
-  name = "home-manager-shell";
-
-  buildInputs = with pkgs; [
-    niv
-    (import sources.home-manager {inherit pkgs;}).home-manager
-  ];
-
   shellHook = ''
-    export NIX_PATH="nixpkgs=${nixpkgs}:home-manager=${sources."home-manager"}"
-    export HOME_MANAGER_CONFIG="./home.nix"
+    export NIX_PATH="nixpkgs=${nixpkgs-src}:home-manager=${home-manager-src}"
+    trap 'rm -f "$TMPFILE"' EXIT
+    TMPFILE=$(mktemp) || exit 1
+    export HOME_MANAGER_CONFIG="$TMPFILE"
+    cat <<HOMENIX >$TMPFILE
+    ${home.nix}
+    HOMENIX
+    ${home-manager}/bin/home-manager build
+    exit 0
   '';
 
 }
